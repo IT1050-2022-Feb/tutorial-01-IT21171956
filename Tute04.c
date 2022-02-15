9c670f282f6378cdaf238ef50268cb6154d0c9f9
IT21171956 <99741436+IT21171956@users.noreply.github.com>
2022-02-15 20:51:19 +0530

    Update Tute04.c

--- Tute04.c
+++ Tute04.c
@@ -17,3 +17,40 @@ int main() {
    printf("%d ", multiply(no1, no2));
    return 0;
 }
+
+#include <stdio.h>
+
+int max(int no1, int no2);
+int min(int no1, int no2);
+int multi (int no1,int no2);
+
+int main() {
+	int no1, no2, maximum, minimum, multiply;
+
+	printf("Enter a value for no 1 : ");
+	scanf("%d", &no1);
+	printf("Enter a value for no 2 : ");
+	scanf("%d", &no2);
+
+	maximum = max(no1, no2);
+	minimum = min(no1, no2);
+	multiply = multi(no1,no2);
+
+	printf("\nMaximum = %d", maximum);
+	printf("\nMinimum = %d", minimum);
+	printf("\nMultiply = %d", multiply);
+
+	return 0;
+}
+
+int max(int no1, int no2) {
+	return (no1 > no2) ? no1 : no2;
+}
+
+int min(int no1, int no2) {
+	return (no1 > no2) ? no2 : no1;
+}
+
+int multi(int no1, int no2) {
+	return (no1* no2);
+}
